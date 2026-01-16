22ba7356880aef6c25bbf0bda736be81610d5960
Imdumb.exe <153335286+imdum12312@users.noreply.github.com>
2026-01-16 01:01:15 -0600

    Add files via upload

---
+++ public/ultraworker.js
@@ -0,0 +1,31 @@
+importScripts("scram/scramjet.all.js");
+
+
+
+if (navigator.userAgent.includes("Firefox")) {
+  Object.defineProperty(globalThis, "crossOriginIsolated", {
+    value: true,
+    writable: true,
+  })
+}
+
+const { ScramjetServiceWorker } = $scramjetLoadWorker();
+const scramjet = new ScramjetServiceWorker();
+
+self.addEventListener("install", () => {
+  self.skipWaiting()
+})
+
+async function handleRequest(event) {
+  await scramjet.loadConfig()
+  if (scramjet.route(event)) 
+    return scramjet.fetch(event)
+  
+  
+  return await fetch(event.request)
+}
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event))
+})
+
